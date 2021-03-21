2bd1592e062911d7063edcca9c3e536db1919a54
Vitor Muniz <vitorfmuniz@gmail.com>
2021-03-21 18:10:31 -0300

    Add minimal plugin

---
+++ __init__.py
@@ -0,0 +1,31 @@
+#-----------------------------------------------------------
+# Copyright (C) 2021 Vitor Muniz
+#-----------------------------------------------------------
+# Licensed under the terms of GNU GPL 3
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#---------------------------------------------------------------------
+
+from PyQt5.QtWidgets import QAction, QMessageBox
+
+def classFactory(iface):
+    return GeoHubPlugin(iface)
+
+class GeoHubPlugin:
+    def __init__(self, iface):
+        self.iface = iface
+
+    def initGui(self):
+        self.action = QAction('Go!', self.iface.mainWindow())
+        self.action.triggered.connect(self.run)
+        self.iface.addToolBarIcon(self.action)
+
+    def unload(self):
+        self.iface.removeToolBarIcon(self.action)
+        del self.action
+
+    def run(self):
+        QMessageBox.information(None, 'GeoHubPlugin', 'aqui é meu plugin')
